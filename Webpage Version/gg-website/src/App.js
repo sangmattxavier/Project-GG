a24e4906f8c5a585d5b050e30d29a41338c538e0
Xavier La Rosa <xavier@Xaviers-MBP.fios-router.home>
2019-06-24 21:58:42 -0400

    Setup React Folder Done

---
+++ Webpage Version/gg-website/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+export default App
