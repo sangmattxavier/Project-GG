516f86e93a96dead399caf6b4f3f455bfaa3d871
Sang Nguyen <sangnguyenpublic@gmail.com>
2019-06-27 22:28:58 -0400

    Set up Landing Page

---
+++ Webpage Version/project-gg/src/LandingPage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Logo from './Logo';
+
+class LandingPage extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="landPage">
+                <div>
+                    <Logo color={white}/>
+                </div>
+                <div className="content">
+                    <img />
+                </div>
+            </div>
+        );
+    }
+}
+export default LandingPage;
