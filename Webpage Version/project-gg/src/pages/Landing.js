29e90905a3eea84798c969b07c6007c7c514d2db
Sang Nguyen <sangnguyenpublic@gmail.com>
2019-06-29 00:16:06 -0400

    Finished first section of Landing

--- Webpage Version/project-gg/src/pages/Landing.js
+++ Webpage Version/project-gg/src/pages/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Logo from "../components/Logo";
+import ControlButton from "../components/ControlButton";
+import "../styles/landing.css";
 
 class Landing extends Component {
   constructor() {
@@ -8,11 +10,20 @@ class Landing extends Component {
   }
   render() {
     return (
-      <div className="homePage">
+      <div id="landingPage">
         <div>
           <Logo color={"white"} />
+          <div id="landHead">
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+            <a href="#faq">FAQ</a>
+          </div>
         </div>
-        <div className="content">TEST</div>
+        <div className="content">
+          <ControlButton label="Register" id="register"/>
+          <img src={require("../Assets/ControllerUncut@2x.png")} id="controller"/>
+          <ControlButton label="Login" id="login"/>
+        </div>
       </div>
     )
   }
