4d39ba711e5c4fca214910582756a7ab8fa53a5b
Matthew Lee <MLeeW2015@gmail.com>
2019-06-28 20:22:12 -0400

    Started working on Login Screen

--- Webpage Version/project-gg/src/pages/Login.js
+++ Webpage Version/project-gg/src/pages/Login.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react"
-import Logo from "../components/Logo"
+import CreateAccountObject from "../components/CreateAccountObject"
+import AccountSubmitButton from "../components/AccountSubmitButton"
+import { Link } from "react-router-dom"
 
+import Logo from "../components/Logo"
+import BackButton from "../components/BackButton"
+
+import "../styles/loginAndSignup.css"
 class Login extends Component {
   constructor() {
     super()
@@ -12,7 +18,14 @@ class Login extends Component {
         <div>
           <Logo color={"white"} />
         </div>
-        <div className="content">Login!</div>
+          <div>
+          <form className="AccountObject">
+            <p className="AccountTitle">Login</p>
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Password" />
+            <AccountSubmitButton label="Login" />
+          </form>
+        </div>
       </div>
     )
   }
