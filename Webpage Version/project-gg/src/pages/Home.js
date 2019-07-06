cd846d147e138a4a57308145d6ed89eeded19564
Xavier La Rosa <xavier@Xaviers-MBP.fios-router.home>
2019-07-06 00:54:07 -0400

    setup basics of navbar for home page and added icon assets

--- Webpage Version/project-gg/src/pages/Home.js
+++ Webpage Version/project-gg/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import "../styles/Home.css"
+import { Link } from "react-router-dom"
 
 class Home extends Component {
   constructor() {
@@ -9,7 +10,54 @@ class Home extends Component {
   render() {
     return (
       <div className="HomeContainer">
-        <div className="HomeNavBar">My Nav Bar</div>
+        <div className="HomeNavBar">
+          <p className="HomeLogoText">GG</p>
+          <form>
+            <input
+              type="text"
+              className="HomeSearchbar"
+              placeholder="Explore..."
+            />
+          </form>
+          <div className="HomeNavButtons">
+            <button className="HomeNavProfile" />
+            <button className="HomeNavButton">
+              <img
+                src={require("../Assets/icons/castle-90.png")}
+                className="HomeIconImage"
+              />
+            </button>
+            <button className="HomeNavButton">
+              <img
+                src={require("../Assets/icons/activity-100.png")}
+                className="HomeIconImage"
+              />
+            </button>
+            <button className="HomeNavButton">
+              <img
+                src={require("../Assets/icons/mailbox-100.png")}
+                className="HomeIconImage"
+              />
+            </button>
+            <button className="HomeNavButton">
+              <img
+                src={require("../Assets/icons/settings-100.png")}
+                className="HomeIconImage"
+              />
+            </button>
+
+            <button className="HomeNavButton">
+              <Link to="/">
+                {" "}
+                <img
+                  src={require("../Assets/icons/logout-100.png")}
+                  className="HomeIconImage"
+                />{" "}
+              </Link>
+            </button>
+          </div>
+        </div>
+
         <div className="HomeContent">
           <div className="HomeEventsAndGroupsColumn">
             <div className="HomeEvents">Events</div>
