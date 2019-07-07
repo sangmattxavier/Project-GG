3553ec24bcd49696f883a3c59083b9cb25c99caf
Xavier La Rosa <xavier@Xaviers-MBP.home>
2019-07-07 12:05:14 -0400

    Huge update on Home page
    Added more components and built more of the basis of the home page
    
    Note: this was dirty coded and I will refactor afterwards

--- Webpage Version/project-gg/src/pages/Home.js
+++ Webpage Version/project-gg/src/pages/Home.js
@@ -91,9 +91,69 @@ class Home extends Component {
                 </li>
               </ul>
             </div>
-            <div className="HomeGroups">Groups</div>
+            <div className="HomeGroups">
+              <div className="HomeEvents">
+                <label className="eventsTitle">
+                  <p>Groups</p>
+                  <img
+                    src={require("../Assets/icons/groups-48.png")}
+                    className="HomeIconImage"
+                  />
+                </label>
+                <ul className="eventsList">
+                  <li>
+                    <label>NJ Smash Group</label>
+                    <p>Admin</p>
+                  </li>
+                  <li>
+                    <label>CSGO West Coast</label>
+                    <p>Member</p>
+                  </li>
+                  <li>
+                    <label>College Gamers</label>
+                    <p>Member</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="HomeMainContentColumn">Main Content</div>
+          <div className="HomeMainContentColumn">
+            <div className="HomeSpicyPostSection">
+              <label className="spicyPostTitle">
+                <img
+                  src={require("../Assets/icons/fire-100.png")}
+                  className="SpicyPostIconImage"
+                />
+                <p>Spiciest Posts of the Day!</p>
+              </label>
+              <div className="spicyPostContent">
+                This is where we will put the spicy post content
+              </div>
+            </div>
+            <div className="HomeSpicyPostSection">
+              <label className="spicyPostTitle">
+                <img
+                  src={require("../Assets/icons/post-96.png")}
+                  className="SpicyPostIconImage"
+                />
+                <p>New Post</p>
+              </label>
+              <div className="spicyPostContent">
+                <form className="Post">
+                  <input
+                    type="text"
+                    placeholder="Post Here..."
+                    className="PostInput"
+                  />
+                </form>
+              </div>
+            </div>
+            <div className="HomeSpicyPostSection">
+              <label className="spicyPostTitle">
+                <p>News Feed</p>
+              </label>
+            </div>
+          </div>
           <div className="HomeFriendsColumn">Friends</div>
         </div>
       </div>
