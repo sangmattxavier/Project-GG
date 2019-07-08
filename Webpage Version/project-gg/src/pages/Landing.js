a001fe8af68d7ba257cf8dc7f1d61d1495fcd076
Sang Nguyen <sangnguyenpublic@gmail.com>
2019-07-07 22:03:50 -0400

    Finished More Sections

--- Webpage Version/project-gg/src/pages/Landing.js
+++ Webpage Version/project-gg/src/pages/Landing.js
@@ -13,7 +13,7 @@ class Landing extends Component {
     return (
       <div id="landingPage">
         <div id="landHeader">
-          <Logo color={"white"} />
+          <a href="#top"><Logo color={"white"} /></a>
           <div id="headLinks">
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
@@ -34,16 +34,57 @@ class Landing extends Component {
           </Link>
         </section>
         <section className="window" id="about">
-          <div className="graphic"><img 
-            src={require("../Assets/TwoPeopleHoldingControllers.png")}
-            id="aboutPic"
-            alt="Family"
-          /></div>
+          <div className="graphic">
+            <img 
+              src={require("../Assets/TwoPeopleHoldingControllers.png")}
+              id="aboutPic"
+              alt="Family"
+            />
+          </div>
           <div className="info">
             <div className="infoHead">What is GG?</div>
             <div className="infoDesc">
               GG is a social media platform where users can share video-game 
               related content to each other and earn experience for sharing that content.
+            </div>
+          </div>
+        </section>
+        <section className="window" id="about2">
+          <div className="graphic">
+            <img
+              src={require("../Assets/GamingEquipment.png")}
+              id="about2Pic"
+              alt="GamingEquipment"
+            />
+          </div>
+          <div className="info">
+            <div className="infoHead">Connect With Any User</div>
+            <div className="infoDesc">
+              Using our platform, you can share art, music, game information, and many more.
+              You will connect to many users, and sometimes these users can be cool, but
+              plenty of times, they will be racist, and sometimes that's life, and it's
+              tough. Like really tough, but we will all get through it in the end my
+              guy. Thank you for attending my sermon, that'll be $2000 dollars to my
+              Venmo, @kinkywisetheclown.
+            </div>
+          </div>
+        </section>
+        <section className="window" id="faq">
+          <div className="graphic">
+            <img
+              src={require("../Assets/GuyWithHeadset.png")}
+              id="faqPic"
+              alt="GuyWithHeadset"
+            />
+          </div>
+          <div className="info">
+            <div className="infoHead">FAQ</div>
+            <div className="infoDesc">
+              FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER
+              FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER
+              FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER
+              FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER
+              FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER FILLER
             </div>
           </div>
         </section>
